b67f59c8acf7e789ff48fd166917b7b5f20366a4
Abdelrhman Fikri <108499713+abdelrhman2148@users.noreply.github.com>
2024-10-24 13:12:51 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
-"""Request caching and tracking"""
-
-
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
 
-cli = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """Gets the HTML content and caches it with expiry"""
-    count_key = f"count:{url}"
-    cli.incr(count_key)
-
-    response = requests.get(url)
-    hc = response.text
-
-    cache_key = f"content:{url}"
-    cli.setex(cache_key, 10, hc)
-
-    return hc
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
